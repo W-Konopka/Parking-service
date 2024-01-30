147f1a6ddcd4fab3c190d0b0af6ad62822ce44bd
W-Konopka <158211618+W-Konopka@users.noreply.github.com>
2024-01-30 20:33:29 +0100

    Add files via upload

---
+++ FileLibrary/FileSaver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using System.Linq;
+
+namespace FileLibrary
+{
+    public class FileSaver
+    {
+        public static void SaveFileToXml<T>(List<T> list, string name)
+        {
+
+            Stream stream = File.Create(Environment.CurrentDirectory + name);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<T>));
+            serializer.Serialize(stream, list);
+            stream.Close();
+
+
+        }
+        public static List<T> ReadFromXml<T>(string name)
+        {
+            List<T> list = new List<T>();
+            if (File.Exists(name))
+            {
+                string path = Path.Combine(Environment.CurrentDirectory, name);
+                XmlSerializer serializer = new XmlSerializer(typeof(List<T>));
+                using (FileStream stream = File.Open(path, FileMode.Open))
+                {
+                    list = (List<T>)serializer.Deserialize(stream);
+                }
+                return list;
+            }
+            return list;
+
+        }
+
+    }
+}
